8f9a8cdb9684ea899c7b90bfdfb19f4bab5b2b5f
doteater <c0ry42@gmail.com>
2025-03-05 13:51:05 -0700

    Create myofficeinsecure.ps1

---
+++ myofficeinsecure.ps1
@@ -0,0 +1,98 @@
+$scriptName = 'myofficeinsecure'
+Start-Transcript "C:\windows\temp\$scriptName.log"
+$DebugPreference = 'Continue'
+
+
+$tempFile = [System.IO.Path]::GetTempFileName() + '.ps1'
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/doteater/doteater.github.io/refs/heads/master/$scriptName.ps1" -OutFile $tempFile
+
+# Self-Elevation Function
+Function Elevate-Script {
+    If (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
+        $newProcess.Arguments = "-NoProfile -ExecutionPolicy Bypass -File `"$tempFile`""
+        $newProcess.Verb = "runas"
+        [System.Diagnostics.Process]::Start($newProcess) | Out-Null
+        Exit
+    }
+}
+
+# Invoke Self-Elevation
+Elevate-Script
+
+# Your script's main code goes here
+
+####
+# INSTALL OFFICE
+####
+
+write-output "Installing Office..."
+
+#don't do this
+winget settings --enable InstallerHashOverride
+#DON'T DO THIS NONO 
+$process = Start-Process -FilePath "winget" -ArgumentList "install --id Microsoft.Office -e --silent --accept-package-agreements --accept-source-agreements --ignore-security-hash" -NoNewWindow -PassThru #DON'T DO THIS NONO
+
+$process.WaitForExit()
+
+#pause briefly needed?
+start-sleep 5
+
+#yes please
+winget settings --disable InstallerHashOverride
+
+write-output "DONE Installing Office"
+
+
+####
+# ACTIVATE OFFICE
+####
+
+write-output "Activating Office..."
+
+# Change directory to Office16
+$pathx64 = "C:\Program Files\Microsoft Office\Office16"
+$pathx86 = "C:\Program Files (x86)\Microsoft Office\Office16"
+if (Test-Path $pathx64) { Set-Location $pathx64 }
+elseif (Test-Path $pathx86) { Set-Location $pathx86 }
+else { Write-Error "No office installation found, failed to activate"; exit 1 }
+
+# Convert retail to VLK by installing VLK licenses
+Get-ChildItem "..\root\Licenses16\ProPlus2019VL*.xrm-ms" | ForEach-Object {
+    cscript //nologo //B ospp.vbs /inslic:"..\root\Licenses16\$($_.Name)"
+}
+
+# Check and remove existing product key
+$dstatusOutput = cscript //nologo ospp.vbs /dstatus
+$productKeyLine = $dstatusOutput | Select-String -Pattern "Last 5 characters of installed product key"
+if ($productKeyLine) {
+    $productKey = ($productKeyLine -split ":")[1].Trim()
+    $productKey = $productKey.Substring($productKey.Length - 5)
+    Write-Host "Last 5 characters of product key: $productKey"
+    cscript //nologo ospp.vbs /unpkey:$productKey
+}
+
+# Check status again (should be nothing)
+#cscript //nologo ospp.vbs /dstatus
+
+# Add temporary GVLK key
+cscript //nologo ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
+
+# Set KMS info and activate
+cscript //nologo ospp.vbs /sethst:kms.sentracam.net
+cscript //nologo ospp.vbs /setprt:1688
+cscript //nologo ospp.vbs /act
+
+# Banner check and registry updates
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform\0ff1ce15-a989-479d-af46-f275c6370663" -Name "KeyManagementServiceName" -ErrorAction SilentlyContinue
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -Name "KeyManagementServiceName" -Value "kms.sentracam.net"
+
+write-output "DONE Activating Office"
+
+$message = "Office Install and Activation Complete!`nPlease close any remaining command prompt windows.`nReport any problems via IT Support Ticket!"
+$title = "Office Install and Activation Complete"
+# Use Start-Process to run the GUI in the user's context
+Start-Process powershell -ArgumentList @"
+    Add-Type -AssemblyName System.Windows.Forms;
+    [System.Windows.Forms.MessageBox]::Show('$message', '$title');
+"@ -NoNewWindow
