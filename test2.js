e6fbb6001addf1e58b9cc61fb18531421aa829c7
doteater <c0ry42@gmail.com>
2025-05-16 11:08:01 -0700

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <script>
+        alert("Hello, World!");
+        console.log("Hello, World!");
+        document.write("Hello, World!");
+    </script>
+</body>
+</html>
